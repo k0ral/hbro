1293c82669f1b364ae7db61b8174be9acd79b87e
koral <koral att mailoo dott org>
2011-07-15 13:59:44 +0200

    Cleaning.

--- Hbro/Main.hs
+++ Hbro/Main.hs
@@ -4,15 +4,12 @@ module Main where
 import Hbro.Config
 import Hbro.Types
 
-import System.Environment
-
 import Paths_hbro
 -- }}}
 
 main :: IO ()
 main = do
-    uiFile      <- getDataFileName "examples/ui.xml"
-    configHome  <- getEnv "XDG_CONFIG_HOME"
+    uiFile <- getDataFileName "examples/ui.xml"
 
 --     putStrLn "[WARNING] You are running the default configuration which provides hardly no feature."
 --     putStrLn $ "[WARNING] You should copy the example configuration files hbro.hs and ui.xml in " ++ configHome ++ "/hbro and start hacking them."
