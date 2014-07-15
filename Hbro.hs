f09250f75d5662e1563cd93d1670e4e8f7e3efb2
koral <koral att mailoo dott org>
2014-07-15 12:59:46 +0200

    Clean-up.

--- Hbro.hs
+++ Hbro.hs
@@ -1,57 +1,29 @@
-module Hbro
-    ( module Hbro.Boot
-    -- , module Hbro.Clipboard
-    -- , module Hbro.Config
-    , module Hbro.Core
-    , module Hbro.Error
-    , module Hbro.Gdk.KeyVal
-    -- , module Hbro.Gui
-    , module Hbro.Gui.Buildable
-    -- , module Hbro.Gui.NotificationBar
-    -- , module Hbro.Gui.PromptBar
-    -- , module Hbro.Gui.StatusBar
-    -- , module Hbro.Hooks
-    , module Hbro.IPC
-    -- , module Hbro.IPC.Hooks
-    , module Hbro.K
-    -- , module Hbro.Keys
-    -- , module Hbro.Keys.Model
-    , module Hbro.Keys.Monadic
-    -- , module Hbro.Keys.Signals
-    -- , module Hbro.Options
-    -- , module Hbro.Signals
-    -- , module Hbro.Signals.PromptBar
-    , module Hbro.Util
-    , module Hbro.Webkit.Lifted
-    -- , module Hbro.Webkit.WebSettings
-    -- , module Hbro.WebView.Hooks
-    -- , module Hbro.WebView.Signals
-) where
+module Hbro ( module X) where
 
-import Hbro.Boot
+import Hbro.Boot as X
 -- import Hbro.Clipboard
 -- import Hbro.Config
-import Hbro.Core
-import Hbro.Error
-import Hbro.Gdk.KeyVal
+import Hbro.Core as X
+import Hbro.Error as X
+import Hbro.Gdk.KeyVal as X
 -- import Hbro.Gui
-import Hbro.Gui.Buildable
+import Hbro.Gui.Buildable as X
 -- import Hbro.Gui.NotificationBar
 -- import Hbro.Gui.PromptBar
 -- import Hbro.Gui.StatusBar
 -- import Hbro.Hooks
-import Hbro.IPC
+import Hbro.IPC as X
 -- import Hbro.IPC.Hooks
-import Hbro.K
+import Hbro.K as X
 -- import Hbro.Keys
 -- import Hbro.Keys.Model
-import Hbro.Keys.Monadic
+import Hbro.Keys.Monadic as X
 -- import Hbro.Keys.Signals
 -- import Hbro.Options
 -- import Hbro.Signals
 -- import Hbro.Signals.PromptBar
-import Hbro.Util
-import Hbro.Webkit.Lifted
+import Hbro.Util as X
+import Hbro.Webkit.Lifted as X
 -- import Hbro.Webkit.WebSettings
 -- import Hbro.WebView.Hooks
 -- import Hbro.WebView.Signals
